0abf5ea5aa6df5d1aa8a7d8e041bf45e7654dfa8
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-11-09 10:46:26 +0600

    update link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 
 async function extractAllDatas(currentSection:string) {
-  const res = await fetch(`/api/${currentSection}/get`, {
+  const res = await fetch(`https://portfolio-2024-e6lw.vercel.app/api/${currentSection}/get`, {
     method: "GET",
     cache: "no-store",
   });
